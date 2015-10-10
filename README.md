03dd72eab8d686f50c7d2bed61af2b82980b9cfc
Clayton <claytonkucera@gmail.com>
2015-10-09 20:42:11 -0400

    simplifying node and express app structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # courses-backend
+
+To run the server, navigate to the root directory of the application in the cli and run the command `node app.js`.
--- app.js
+++ app.js
@@ -1,24 +1,43 @@
+//config middleware
 var express = require('express');
-var http = require('http');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var port = process.env.PORT || '3000';
+
 
 var app = express();
 
-var port = process.env.PORT || 3000;
-
-//configure express
-app.set('port', port);
+// view setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//set up initial routing
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname+'/app/index.html'));
-    //res.render('app/index', {});
-});
+//other middleware setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//start the server
-var server = app.listen(port, function() {
-    console.log("listening on port " + port);
-});
+//route setup
+app.use('/', routes);
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.listen(port);
+
 
 module.exports = app;
