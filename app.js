623032fb660c13560c4cc59a0e7771f3891a059d
Clayton <claytonkucera@gmail.com>
2015-11-29 17:10:14 -0500

    changed port number for openshift

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var app = express();
